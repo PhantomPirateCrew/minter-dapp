d10438ed29167d10b3feb6038e1a859c226e1e6f
PhantomPirateCrew <phantompiratecrew@gmail.com>
2022-06-21 15:36:55 -0400

    yes

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 50,
+    growEditionSizeTo: 10,
     layersOrder: [
       { name: "Background" },
       { name: "Body" },
@@ -24,6 +24,7 @@ const layerConfigurations = [
       { name: "Facial Hair" },
       { name: "Pipe" },
       { name: "Weapon" },
+      { name: "Jewelry" },
     ],
   },
 ];
